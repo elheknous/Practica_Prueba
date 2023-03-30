87ec3345fb9bb990af3ee15c790bb6da475bc350
unknown <josematustoro6@gmail.com>
2023-03-30 11:25:37 -0300

    Se añadio el resto de archivos y la funcion suma en el metodo main

---
+++ src/main/java/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println("La suma total es de: "+ suma(a,b));
+    }
+
+    private static int suma(int a, int b) {
+        return a +b;
+    }
+}
