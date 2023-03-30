2de15aa841fc46238391acc61813dcca939d72f1
unknown <josematustoro6@gmail.com>
2023-03-30 11:33:50 -0300

    Se añadio mensaje de despedida

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
         int b = 3;
         System.out.println("La suma total es de: "+ suma(a,b));
         System.out.println("El producto es de: "+multiplicar(a,b));
+        System.out.println("Hasta pronto, gracia por participar :D");
+        System.out.println("######################################");
     }
 
     private static int multiplicar(int a, int b) {
